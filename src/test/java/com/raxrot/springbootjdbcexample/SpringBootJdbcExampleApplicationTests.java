92ee7797116268bd5b32cc69bd533bc2437b29c4
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-10-22 11:13:15 +0100

    first commit

---
+++ src/test/java/com/raxrot/springbootjdbcexample/SpringBootJdbcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.springbootjdbcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
